95cbac137c677c8777d18c62754058b3d8835d55
Bruno Tetzner <brunoltetzner@gmail.com>
2022-02-15 15:58:23 -0300

    bug da funçao remove_more_than_two_repetitions resolvido

--- main.py
+++ main.py
@@ -22,20 +22,20 @@ def remove_more_than_two_repetitions(text):
    new_sentece = text[:2]
    count = 2
    while count < len(text):
-       if text[count] != text[count-1] and text[count] != text[count-2]:
+       if text[count] != text[count-1] or text[count] != text[count-2]:
         new_sentece+=text[count]
-       elif text[count] != text[count-1] and text[count] == text[count-2]:
+       elif text[count] != text[count-2] and text[count] == text[count-1]:
         new_sentece+=text[count]
-    #    elif count == len(text)-1 and text[count] == text[count-1]:
-    #     new_sentece+=text[count]
        elif count == len(text)-1 and text[count] == text[count-1] and text[count] != text[count-2]:
         new_sentece+=text[count]
+       elif count == len(text)-1 and text[count] == text[count-1] and text[count] == text[count-2]:
+        pass
        elif text[count] == text[count-1] and text[count] == text[count+1] and text[count-2] !=text[count]:
         new_sentece+=text[count]
 
        count+=1
    return new_sentece
 
-print(remove_more_than_two_repetitions("Ollloco meuuuu, taaa peegaando fogoo biiiiichooo"))
+print(remove_more_than_two_repetitions("aaaabbbbaacaaaxxxxii"))
 
 
